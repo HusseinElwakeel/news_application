fa7aefd05e02156406bb90dffc663026760dfbaa
Hussein <husseinelwakeel765@gmail.com>
2023-11-13 16:58:20 +0200

    Initial commit Create appBar of the app

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        //to remove the background of elevation
+        elevation: 0,
+        title: Row(
+          // to make the row in center of screen
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "News",
+              style: TextStyle(color: Colors.black, fontSize: 22),
+            ),
+            Text(
+              "Cloud",
+              style: TextStyle(
+                color: Colors.orange,
+                fontSize: 22,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
