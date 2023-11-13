b848fe3f709f8ea2a2dda00a07556fb0f70c2e9b
Hussein <husseinelwakeel765@gmail.com>
2023-11-13 18:12:51 +0200

    to display the card of category using Row and SingleChildScrollView

---
+++ lib/Models/CategoryModel.dart
@@ -0,0 +1,6 @@
+class CategoryModel {
+  final String Image;
+  final String Text;
+  // make it const to not remove const in Category Widget
+  const CategoryModel({required this.Image, required this.Text});
+}
